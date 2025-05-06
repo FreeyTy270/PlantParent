3de828af699da260cd5631bf446409d4186f0fad
Ty Freeman <freemanty27@gmail.com>
2025-05-05 21:40:04 -0400

    Can reliably send data from /existing endpoint to the dashboard and display it. Modifying the styling of the page for funsies.

--- backend/plantparent/api.py
+++ backend/plantparent/api.py
@@ -25,9 +25,22 @@ async def root():
 @app.get("/existing")
 async def existing():
     print("Answering a response!!")
+    plant1 = Plant(nickname="Plant 1", scientific_name="Babymus Plantimus",
+                   check_rate=7)
+    plant2 = Plant(nickname="Plant 2", check_rate=14)
+    plant3 = Plant(nickname="Plant 3", check_rate=21)
+
     current_plants = [
-        Plant(nickname="Plant 1", check_rate=7),
-        Plant(nickname="Plant 2", check_rate=14),
-        Plant(nickname="Plant 3", check_rate=21),
+        plant1.model_dump(),
+        plant2.model_dump(),
+        plant3.model_dump(),
     ]
-    return {current_plants}
+    return current_plants
+
+
+if __name__ == "__main__":
+    import sys
+    import uvicorn
+
+    uvicorn.run("plantparent.api:app", host="0.0.0.0", port=8000, reload=True)
+    sys.exit(0)
